de1dc5345677233ea49b596897eec665765f507a
Marin <mbtakanov@yahoo.com>
2014-05-18 13:32:40 +0300

    Java Console Game

--- BlackJack.java
+++ BlackJack.java
@@ -2,70 +2,270 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+import com.sun.org.apache.bcel.internal.generic.GOTO;
+
 
 public class BlackJack {
+	
+	public static double playerMoney = 500;
+	public static boolean cashOut = false;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		ArrayList<String> cards = new ArrayList<String>();
-		CreateDeck(cards);
 		
-		//Game example for first deal. Must rewrite.
-		//after each deal cards must be removed from the ArrayList
-		int dealerCard1 = randomCard(cards);
-		int dealerCard2 = randomCard(cards);
-		int dealerCount = getCount(cardConverter(dealerCard1)) + getCount(cardConverter(dealerCard2));
-		System.out.println("Dealer have: " + cardConverter(dealerCard1) + " " + "X");
-//		System.out.println(dealerCard1 + " " + dealerCard2);
-		cards.remove(cards.indexOf(dealerCard1 + ""));		
-		cards.remove(cards.indexOf(dealerCard2 + ""));
+		createDeck(cards);
 		
-		int playerCard1 = randomCard(cards);
-		int playerCard2 = randomCard(cards);
-		int playerCount = getCount(cardConverter(playerCard1)) + getCount(cardConverter(playerCard2));
-		System.out.println("You have: " + cardConverter(playerCard1) + " " + cardConverter(playerCard2));
-//		System.out.println(playerCard1 + " " + playerCard2);
-		cards.remove(cards.indexOf(playerCard1 + ""));
-		cards.remove(cards.indexOf(playerCard2 + ""));
-		//End of game example
-		
-		//Game example for the next deals. Must rewrite.
-		System.out.println("Deal or stay? [d/s]");
-		Scanner sc = new Scanner(System.in);
-		String command = sc.next();
-		
-		if(command.equals("d")){
-
-			int playerCard3 = randomCard(cards);
-			playerCount += getCount(cardConverter(playerCard3));
+		while(!cashOut){
 			
-			if(playerCount > 21){
-				System.out.println("You lost!");
-			}
-			else if(playerCount < 21){
-				System.out.println("You have: " + cardConverter(playerCard1) + 
-											" " + cardConverter(playerCard2) + 
-											" " + cardConverter(playerCard3));
-				System.out.println("Deal or stay? [d/s]");
-				String newcommand = sc.next();
-			}
-				
-		}
-		else if(command.equals("s")){
-			if(dealerCount > playerCount){
-				System.out.println("Dealer have: " + cardConverter(dealerCard1) + " " + cardConverter(dealerCard2));
-				System.out.println("You lost!");
-			}
-			else if(dealerCount < playerCount){
-				System.out.println("Dealer have: " + cardConverter(dealerCard1) + " " + cardConverter(dealerCard2));
-				System.out.println("You Win!");
-			}
-		}
-		//End of game example
+        	playerMoney = deal(cards, playerMoney);
+        	
+        	if(cards.size() < 104){
+            	createDeck(cards);
+            }
+        	
+        	if(playerMoney == 0 ){
+            	System.out.println("\n\n\n\n\n\n");
+            	System.out.println("\t\t\t\t\t\t\t\t Game Over");
+            	System.out.println("\n\n\n\n\n\n\n");
+            	break;
+            }
+            else if(playerMoney == 0.5){
+            	System.out.println("\n\n\n\n\n\n");
+            	System.out.println("\t\t\t\t\t\t\t  Thank you for the tip!");
+            	System.out.println("\t\t\t\t\t\t\t\t Game Over");
+            	System.out.println("\n\n\n\n\n\n\n");
+            	break;
+            }
+        }
+		
+		if(cashOut){
+    		System.out.println("\n\n\n\n\n\n");
+        	System.out.printf("\t\t\t\t\t Congratulations! You have $%.2f! Have a nice day!", playerMoney);
+        	System.out.println("\n\n\n\n\n\n\n\n");
+    	}
 	}
 	
+	public static double deal(ArrayList<String> cards, double playerMoney) throws InterruptedException {
+		
+		System.out.println("Write 'take all' for cash out!");
+		System.out.printf("You have: $%.2f \n", playerMoney);
+		Scanner in = new Scanner(System.in);
+		
+		double bet = 0.0;
+		
+    	while(true) {
+    		
+    		System.out.print("What is your bet: ");
+    		String checker = in.nextLine();
+    		
+    		if(checker.contains("take all")){
+    			cashOut = true;
+    			break;
+    		}
+    		//Check is the input a number.
+    		else if(checker.matches("\\d+")){ 
+    			
+    			bet = Double.parseDouble(checker);
+    			bet = (double) Math.round(bet);
+    			
+    			if(bet == 0) {
+    				System.out.println("You must bet!");
+    			} else if(bet < 0) {
+    				System.out.println("You can't be serious!");
+    			} else if(bet > playerMoney) {
+    				System.out.println("You don't hava enough money!");
+    			}else {
+    				break;
+    			}
+    			
+    		} else {
+    			continue;
+    		} 
+		}
+    	
+    	while(!cashOut){
+	    	System.out.printf("You bet: $%.2f \n", bet);
+	    	System.out.println();
+	    	
+			int playerCard1 = randomCard(cards);
+			cards.remove(cards.indexOf(playerCard1 + ""));
+			int playerCard2 = randomCard(cards);
+			cards.remove(cards.indexOf(playerCard2 + ""));
+			int playerCount = getCount(cardConverter(playerCard1)) + getCount(cardConverter(playerCard2));
+			System.out.printf("You have(%d):  %s  %s \n", playerCount, cardConverter(playerCard1), cardConverter(playerCard2));
+			
+			int dealerCard1 = randomCard(cards);
+			cards.remove(cards.indexOf(dealerCard1 + ""));
+			int dealerCard2 = randomCard(cards);
+			cards.remove(cards.indexOf(dealerCard2 + ""));
+			int dealerCount = getCount(cardConverter(dealerCard1)) + getCount(cardConverter(dealerCard2));
+			System.out.printf("Dealer have(%d): %s X \n", getCount(cardConverter(dealerCard1)), cardConverter(dealerCard1));
+			
+			String playerCards = cardConverter(playerCard1) + " " + cardConverter(playerCard2);
+			String playerCardsCopy = cardConverter(playerCard1) + cardConverter(playerCard2);
+			
+			String dealerCards = " " ;
+			String dealerCardsCopy = cardConverter(dealerCard1) + cardConverter(dealerCard2);
 	
-	public static void CreateDeck(ArrayList<String> cards){
+			while(true){
+	
+				//Check for Black Jack
+				if((playerCards.contains("A") && (playerCards.contains("10") ||
+												  playerCards.contains("J")  ||
+												  playerCards.contains("K")  ||
+												  playerCards.contains("Q")) && playerCount == 21) &&
+					(getCount(cardConverter(dealerCard1)) != 10 && getCount(cardConverter(dealerCard1)) != 11) ){
+					
+					System.out.println("Black Jack! You win! \n");
+					playerMoney += bet * 1.5;
+					Thread.sleep(3000);
+					break;
+				}
+				else if((playerCards.contains("A") && (playerCards.contains("10") ||
+													   playerCards.contains("J")  ||
+													   playerCards.contains("K")  ||
+													   playerCards.contains("Q")) && playerCount == 21) && 
+						(getCount(cardConverter(dealerCard1)) == 10 || getCount(cardConverter(dealerCard1)) == 11) ){
+					
+					dealerCards = cardConverter(dealerCard1) + " " + cardConverter(dealerCard2);
+					if(dealerCount != 21){
+						System.out.printf("Dealer have(%d): %s \n", dealerCount, dealerCards);
+						System.out.println("Black Jack! You win! \n");
+						playerMoney += bet * 1.5;
+						Thread.sleep(3000);
+						break;
+					}
+					else if(dealerCount == 21){
+						System.out.printf("Dealer have(%d): %s \n", dealerCount, dealerCards);
+						System.out.println("Draw!");
+						Thread.sleep(3000);
+						break;
+					}
+				}
+				
+				//Two Aces case
+				if(playerCardsCopy.contains("A") && playerCount > 21){
+					playerCount -= 10;
+					StringBuilder strBuilder = new StringBuilder(playerCardsCopy);
+					strBuilder.deleteCharAt(playerCardsCopy.indexOf("A"));
+					playerCardsCopy = strBuilder.toString();
+				}
+				
+				System.out.println("Deal or stay? [d/s]");
+				String command = in.next();
+				
+				if(command.equals("d")){
+					Thread.sleep(1000);
+					dealerCards = cardConverter(dealerCard1) + " X" ;
+					
+					int playerNextCard = randomCard(cards);
+					cards.remove(cards.indexOf(playerNextCard + ""));
+					playerCount += getCount(cardConverter(playerNextCard));
+					
+					playerCards += " " + cardConverter(playerNextCard);
+					playerCardsCopy += cardConverter(playerNextCard);
+	
+					if(playerCardsCopy.contains("A") && playerCount > 21){
+							playerCount -= 10;
+							StringBuilder strBuilder = new StringBuilder(playerCardsCopy);
+							strBuilder.deleteCharAt(playerCardsCopy.indexOf("A"));
+							playerCardsCopy = strBuilder.toString();
+					}
+					
+					if(playerCount > 21){
+						System.out.printf("You have(%d): %s \n", playerCount, playerCards);
+						System.out.printf("Too many!");
+						playerMoney-=bet;
+						Thread.sleep(2000);
+						break;
+					}
+					else if(playerCount <= 21){
+						System.out.printf("You have(%d): %s \n", playerCount, playerCards);
+						continue;
+					}	
+				}
+				
+				else if(command.equals("s")){
+					
+					Thread.sleep(1000);
+					dealerCards = cardConverter(dealerCard1) + " " + cardConverter(dealerCard2);
+					
+					if(dealerCardsCopy.contains("A") && dealerCount > 21){
+						dealerCount -= 10;
+						StringBuilder strBuilder = new StringBuilder(dealerCardsCopy);
+						strBuilder.deleteCharAt(dealerCardsCopy.indexOf("A"));
+						dealerCardsCopy = strBuilder.toString();
+					}
+					
+					for (int i = 0; i < 1; i++) {
+						System.out.printf("You have(%d): %s \n", playerCount, playerCards);
+						System.out.printf("Dealer have(%d): %s \n", dealerCount, dealerCards);
+						
+					}
+					
+					while(true){
+						
+						if(dealerCount < 17){
+							Thread.sleep(3000);
+							int dealerNextCard = randomCard(cards);
+							cards.remove(cards.indexOf(dealerNextCard + ""));
+							dealerCount += getCount(cardConverter(dealerNextCard));
+							
+							dealerCards += " " + cardConverter(dealerNextCard);
+							dealerCardsCopy += cardConverter(dealerNextCard);
+							
+							if(dealerCardsCopy.contains("A") && dealerCount > 21){
+								dealerCount -= 10;
+								StringBuilder strBuilder = new StringBuilder(dealerCardsCopy);
+								strBuilder.deleteCharAt(dealerCardsCopy.indexOf("A"));
+								dealerCardsCopy = strBuilder.toString();	
+							}
+							
+							System.out.printf("Dealer have(%d): %s \n", dealerCount, dealerCards);
+							
+							if(dealerCount < 17){
+								continue;
+							}
+							else if(dealerCount > 21){
+								System.out.println("You win!");
+								playerMoney+=bet;
+								break;
+							}
+							else if(dealerCount >= 17){
+								continue;
+							}
+							Thread.sleep(1000);
+						}
+						
+						if(dealerCount >= 17){
+							if(dealerCount > playerCount){
+								System.out.println("You lost!");
+								playerMoney-=bet;
+								break;
+							}
+							else if(dealerCount == playerCount){
+								System.out.println("Draw");
+								break;
+							}
+							else if(dealerCount < playerCount){
+								System.out.println("You win!");
+								playerMoney+=bet;
+								break;
+							}
+						}
+					}
+					Thread.sleep(4000);
+					break;
+				}
+			}
+			break;
+	    }
+		System.out.println();
+		return playerMoney;
+	} //End of deal method
+	
+	public static void createDeck(ArrayList<String> cards){
 		
 		cards.removeAll(cards);
 		
@@ -125,61 +325,41 @@ public class BlackJack {
 		int symbolType = card % 10;
 		switch(symbolType)
 		{
-			case 1: typeOfSymbol = '♠'; break;
-			case 2: typeOfSymbol = '♥'; break;
-			case 3: typeOfSymbol = '♣'; break;
-			case 4: typeOfSymbol = '♦'; break;
+			case 1: typeOfSymbol = '\u2660'; break;
+	        case 2: typeOfSymbol = '\u2665'; break;
+	        case 3: typeOfSymbol = '\u2663'; break;
+	        case 4: typeOfSymbol = '\u2666'; break;
 		}
 
 		finalCard = typeOfCard + typeOfSymbol;
 		return finalCard;
 	}
 	
-	public static int getCount(String card){ //Count method. Must rewrite for the Ace - 1 or 11.
+	public static int getCount(String card){
 		
 		int count = 0;
-		if (card.contains("A"))
-		{
-			count = 11;
+        if (contains(card, "A")){
+            count = 11;
+        }
+        else if (contains(card, "J") || contains(card, "Q") || contains(card, "K")){
+            count = 10;
+        }
+        
+        for (int i = 2; i <= 10; i++) {
+			if(contains(card, i + "")){
+				count = i;
+			}
 		}
-		else if(card.contains("2"))
-		{
-			count = 2;
-		}
-		else if(card.contains("3"))
-		{
-			count = 3;
-		}
-		else if(card.contains("4"))
-		{
-			count = 4;
-		}
-		else if(card.contains("5"))
-		{
-			count = 5;
-		}
-		else if(card.contains("6"))
-		{
-			count = 6;
-		}
-		else if(card.contains("7"))
-		{
-			count = 7;
-		}
-		else if(card.contains("8"))
-		{
-			count = 8;
-		}
-		else if(card.contains("9"))
-		{
-			count = 9;
-		}
-		else if(card.contains("10") || card.contains("J") || card.contains("Q") || card.contains("K"))
-		{
-			count = 10;
-		}
-		return count;
+      
+        return count;
 	}
-	
-	
+	public static boolean contains(String card, String input){
+
+    	if(card.contains(input)){
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+
 }
